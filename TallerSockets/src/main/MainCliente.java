16cfc8484d81f32d76c33cc2b54b129b147593bd
JoannZamudio11 <126726984+JoannZamudio11@users.noreply.github.com>
2023-08-17 18:52:42 -0500

    Creación clase cliente y modificación de la lógica del programa
    En esta versión se agregó la clase cliente y se modificó la lógica que conecta el cliente con el servidor para generar un envío de Strings.

---
+++ TallerSockets/src/main/MainCliente.java
@@ -0,0 +1,11 @@
+package main;
+
+public class MainCliente {
+
+	public static void main(String[] args) {
+		Cliente cl = new Cliente();
+		System.out.println("Iniciando cliente...");
+		cl.startCliente();
+	}
+
+}
